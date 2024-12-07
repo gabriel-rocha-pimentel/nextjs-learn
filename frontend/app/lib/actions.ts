365237307a0f50f692877ee81130fd35d61e8f15
Gabriel Rocha Pimentel <gabriel-rocha-pimentel@users.noreply.github.com>
2024-12-06 21:33:05 -0300

    created function to update invoice and send invoice

--- frontend/app/lib/actions.ts
+++ frontend/app/lib/actions.ts
@@ -9,7 +9,7 @@ const formSchema = z.object({
   id: z.string(),
   customerId: z.string(), 
   amount: z.coerce.number(),
-  status: z.enum(['panding', 'paid']),
+  status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
 
@@ -21,22 +21,35 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  const amountInCent =amount * 100;
+  const amountInCents =amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql `
-    INSERT INTO invoices(customerId, amout, status, date)
-    VALUES ${customerId}, ${amountInCent} ${status}, ${date}
+  await sql`
+    INSERT INTO invoices(customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
 
-  const rawFormData = {
+const UpdateInvoice = formSchema.omit({id: true, date: true});
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status} = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
 
-  console.log(rawFormData);
+  const amountInCent = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCent}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
