2f1af29273a0f97d43fb0f8a177c9c4f1e8e33ef
Gabriel Rocha Pimentel <gabriel-rocha-pimentel@users.noreply.github.com>
2024-12-10 16:08:37 -0300

    add metadada in all pages of project

--- frontend/app/dashboard/(overview)/page.tsx
+++ frontend/app/dashboard/(overview)/page.tsx
@@ -7,6 +7,11 @@ import {
   LatestInvoicesSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 import CardWrapper from '@/app/ui/dashboard/cards';
 
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
+import { Metadata } from 'next';
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Default',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, buildt with App Router',
+  metadataBase: new URL('https://web-dashboard-silk.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
