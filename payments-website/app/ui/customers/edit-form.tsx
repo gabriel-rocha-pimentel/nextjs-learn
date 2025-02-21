1b9ccfd4e5d8317533d1f59d512c7f2dba753c2c
gabriel-rocha-pimentel <gabrielrochapimentel.dev@gmail.com>
2025-02-20 22:06:56 -0300

    created new autentications for users

--- payments-website/app/ui/customers/edit-form.tsx
+++ payments-website/app/ui/customers/edit-form.tsx
@@ -64,25 +64,6 @@ export default function EditCustomerForm({
             </div>
           </div>
         </div>
-
-        {/* Customer Image */}
-        <div className="mb-4">
-          <label htmlFor="image_url" className="mb-2 block text-sm font-medium">
-            Customer Image
-          </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              <input
-                id="image_url"
-                name="image_url"
-                type="text"
-                defaultValue={customer.image_url}
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              />
-              <PhotoIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-        </div>
       </div>
 
       <div className="mt-6 flex justify-end gap-4">
